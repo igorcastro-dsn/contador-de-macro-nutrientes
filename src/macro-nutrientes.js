5bb147434bd40d2b6734e7141e5ec78e16287b98
igorcastro-dsn <igorcastro.dsn@gmail.com>
2020-09-08 00:26:14 -0300

    REMOVE console.log

--- src/macro-nutrientes.js
+++ src/macro-nutrientes.js
@@ -45,7 +45,6 @@ export class MacroNutrientes extends LitElement {
 
     _caloriasTotais = (e) => {
         this.caloriasTotais = e.detail;
-        console.log(this.caloriasTotais)
         
         if (this.caloriasTotais == 0.00) {
             this._inicializarVariaveis();
